afb1815f8293b22a99429e239ad24885596c74db
Hellmann, Tobias <Tobias.Hellmann@neos-it.de>
2016-08-26 16:24:16 +0200

    ADI-326 - change prefix and i18n values

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ if (!defined('ABSPATH')) {
 }
 
 define('NEXT_AD_INT_PLUGIN_VERSION', '2.0');
-define('NEXT_AD_INT_PREFIX', 'adi2_');
+define('NEXT_AD_INT_PREFIX', 'next_ad_int_');
 define('NEXT_AD_INT_PATH', dirname(__FILE__));
 define('NEXT_AD_INT_URL', plugins_url('', __FILE__));
-define('NEXT_AD_INT_I18N', 'ad-integration-2.0');
+define('NEXT_AD_INT_I18N', 'next_ad_int');
 define('NEXT_AD_INT_PLUGIN_FILE', plugin_basename(__FILE__));
 
 require_once NEXT_AD_INT_PATH . '/Autoloader.php';
