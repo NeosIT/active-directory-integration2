8fa6fbe50e3c16f3806e58448206e2e6240ec0ff
Hellmann, Tobias <Tobias.Hellmann@neos-it.de>
2016-08-30 16:16:49 +0200

    - fix missing `please purchase support license` message

--- constants.php
+++ constants.php
@@ -9,4 +9,4 @@ define('NEXT_AD_INT_PREFIX', 'next_ad_int_');
 define('NEXT_AD_INT_PATH', dirname(__FILE__));
 define('NEXT_AD_INT_URL', plugins_url('', __FILE__));
 define('NEXT_AD_INT_I18N', 'next_ad_int');
-define('NEXT_AD_INT_PLUGIN_FILE', plugin_basename(__FILE__));
+define('NEXT_AD_INT_PLUGIN_FILE', plugin_basename(NEXT_AD_INT_PATH . '/index.php'));
