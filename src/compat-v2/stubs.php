da6e2043fef200b3364f463ef5256d8e2b8b8a08
Christopher Klein <ckl@dreitier.com>
2023-03-06 21:15:09 +0100

    fix: Postpone stub creation in favor of other autoloaders (#181)
    Signed-off-by: Christopher Klein <ckl@dreitier.com>

--- src/compat-v2/stubs.php
+++ src/compat-v2/stubs.php
@@ -1,36 +1,56 @@
 <?php
-if (!class_exists('NextADInt_Adi_Init')) {
-	class NextADInt_Adi_Init
-	{
+/**
+ * Create stubs for classes which are required by older NADI PEs.
+ * @return void
+ */
+function next_ad_int_create_class_stubs()
+{
+	if (!class_exists('NextADInt_Adi_Init')) {
+		class NextADInt_Adi_Init
+		{
 
+		}
 	}
-}
 
-if (!class_exists('NextADInt_Core_Logger')) {
-	class NextADInt_Core_Logger extends \Dreitier\Nadi\Log\NadiLog {
+	if (!class_exists('NextADInt_Core_Logger')) {
+		class NextADInt_Core_Logger extends \Dreitier\Nadi\Log\NadiLog
+		{
+		}
 	}
+
+	if (!class_exists('Logger')) {
+		require_once __DIR__ . '/logger.php';
+	}
+
+	if (!class_exists('\Monolog\Registry')) {
+		require_once __DIR__ . '/monolog_registry.php';
+	}
+
+	// BEGIN: compat nadiext-buddypress-simpleattributes
+	if (!defined('NEXT_AD_INT_PREFIX')) {
+		define('NEXT_AD_INT_PREFIX', NEXT_ACTIVE_DIRECTORY_INTEGRATION_PREFIX);
+	}
+	// END
+
+	// BEGIN: nadiext-wp-cli
+	if (!class_exists('\Monolog\Handler\NullHandler')) {
+		require_once __DIR__ . '/monolog_handler_nullhandler.php';
+	}
+
+	if (!class_exists('\Monolog\Logger')) {
+		require_once __DIR__ . '/monolog_logger.php';
+	}
+	// END
 }
 
-if (!class_exists('Logger')) {
-	require_once __DIR__ . '/logger.php';
+// when running in WordPress, we postpone the stub creation so that every other plug-in has the chance to call its autoloader.
+// this prevents issues like #181: the-events-calendar can initialize its non-repackaged Monolog instance and we can use it.
+if (function_exists('add_action')) {
+	add_action('plugins_loaded', function () {
+		next_ad_int_create_class_stubs();
+	});
 }
-
-if (!class_exists('\Monolog\Registry')) {
-	require_once __DIR__ . '/monolog_registry.php';
+// when running unit tests, initialize stubs directly
+else {
+	next_ad_int_create_class_stubs();
 }
-
-// BEGIN: compat nadiext-buddypress-simpleattributes
-if (!defined('NEXT_AD_INT_PREFIX')) {
-	define('NEXT_AD_INT_PREFIX', NEXT_ACTIVE_DIRECTORY_INTEGRATION_PREFIX);
-}
-// END
-
-// BEGIN: nadiext-wp-cli
-if (!class_exists('\Monolog\Handler\NullHandler')) {
-	require_once __DIR__ . '/monolog_handler_nullhandler.php';
-}
-
-if (!class_exists('\Monolog\Logger')) {
-	require_once __DIR__ . '/monolog_logger.php';
-}
-// END
